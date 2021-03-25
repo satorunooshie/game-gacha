a6e63ab164627450df84c2d0d93957b2f7300dba
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-03-25 20:55:31 +0900

    [test]サービス層のコレクション一覧のテスト実装

--- pkg/server/service/service_test.go
+++ pkg/server/service/service_test.go
@@ -6,11 +6,15 @@ import (
 )
 
 type mockRepository struct {
-	userRepository *mock_model.MockUserRepositoryInterface
+	userRepository               *mock_model.MockUserRepositoryInterface
+	collectionItemRepository     *mock_model.MockCollectionItemRepositoryInterface
+	userCollectionItemRepository *mock_model.MockUserCollectionItemRepositoryInterface
 }
 
 func newMockRepository(ctrl *gomock.Controller) *mockRepository {
 	return &mockRepository{
-		userRepository: mock_model.NewMockUserRepositoryInterface(ctrl),
+		userRepository:               mock_model.NewMockUserRepositoryInterface(ctrl),
+		collectionItemRepository:     mock_model.NewMockCollectionItemRepositoryInterface(ctrl),
+		userCollectionItemRepository: mock_model.NewMockUserCollectionItemRepositoryInterface(ctrl),
 	}
 }
